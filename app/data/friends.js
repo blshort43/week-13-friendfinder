3525d1316175dae7370f2235044336fbb4d06713
Brandon <blshort43@gmail.com>
2016-10-26 07:24:17 -0500

    created express app and paths

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+{
+  "name": "Brandon",
+  "photo": "https://lh3.googleusercontent.com/-oNw_C69nPTM/VU5uwrsgmqI/AAAAAAAAHhY/kBuEenc-_mMAoAB2kug1GO3X0cZbD-fHQCEw/w280-h280-p/382a8a5.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+}
+];
+
+module.exports = (friends);
--- server.js
+++ server.js
@@ -3,3 +3,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.static('app/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
+
+app.listen(PORT, function(err, res) {
+	if (err) throw err;
+    console.log('App listening on PORT ' + PORT);
+});
